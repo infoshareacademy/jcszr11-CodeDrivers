acd38ef007574967e5ebc2ee110215ff298df511
Jan Polak <jan.p.polak@gmail.com>
2023-10-02 21:16:01 +0200

    Preparing to filter and display items

--- CodeDrivers/CodeDrivers/CodeDrivers/ListHelper.cs
+++ CodeDrivers/CodeDrivers/CodeDrivers/ListHelper.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace CodeDrivers
 {
-    internal class Wyświetlanie_listy_aut
+    internal interface IRepository<T>
     {
+        List<T> GetAll();
+        T GetAllAvailable(bool isAvaialable);
+        void DisplayItems(IEnumerable<T> items);
+       
+    }
+    internal class ListHelper : IRepository<Car>
+    {
+        public void DisplayItems(IEnumerable<Car> items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Id, item.BrandName, item.Model, item.IsAutomatic, item.IsAvaiable);
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetAllAvailable(bool isAvaialable)
+        {
+            throw new NotImplementedException();
+        }
     }
     //Lista dodanych samochodów będzie możliwa do wyświetlenia dla admina i usera.
     //Marka, model, kategoria (male, rodzinne, dostawcze), rodzaj paliwa, cena wynajmu/dzien, KM , skrzynia biegow,elektryczne/spalinowe
