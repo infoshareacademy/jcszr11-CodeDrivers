cfc95fed60a71125e03e037a09da1d22c99cb62d
Krzysztof Dabrowski <k.dabrowski87@gmail.com>
2023-10-05 19:35:23 +0200

    initial

---
+++ CodeDrivers/CodeDrivers/CodeDrivers/CarInventoryEditor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDrivers
+{
+    internal class CarInventoryEditor : ICarEditor
+    {
+        private List<Car> CarInventory; 
+
+        public CarInventoryEditor(List<Car> carInventory) { 
+          CarInventory = carInventory;
+        } 
+
+
+        public bool EditBrand(int id, string brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditCategory(int id, string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditFuelType(int id, string fuelType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditGearTYpe(int id, string newGearType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditModel(int id, string model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditMotorPower(int id, int newMotorPower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditMotorType(int id, string newMotorType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditPrice(int id, decimal newPrice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeDrivers/CodeDrivers/CodeDrivers/ICarEditor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDrivers
+{
+    internal interface ICarEditor
+    {
+        public bool EditBrand(int id, string brand);
+        public bool EditModel(int id, string model);
+        public bool EditCategory(int id, string category);
+        public bool EditFuelType(int id, string fuelType);
+        public bool EditPrice(int id, decimal newPrice);
+        public bool EditMotorPower(int id, int newMotorPower);
+        public bool EditGearTYpe(int id, string newGearType);
+        public bool EditMotorType(int id, string newMotorType);
+
+    }
+}
