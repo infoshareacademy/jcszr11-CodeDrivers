5bfef2b1b86ff36aa9ce1ecaf2423f6a996a3d86
Jan Polak <jan.p.polak@gmail.com>
2023-11-07 21:39:13 +0100

    Added CSV File Handling and Password Validation Module

---
+++ CodeDrivers/CodeDrivers/CodeDrivers/CsvHanlder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDrivers
+{
+    internal class CsvHanlder
+    {
+        private string filePath = @"C:\Users\Janek\Desktop\FakeCrudentials.csv";
+        public List<string> GetCredentialsFromFile(string path)
+        {
+            var credentials = new List<string>();
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                credentials.AddRange(lines);
+
+                return credentials;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Wystąpił błąd podczas odczytu pliku CSV: " + ex.Message);
+                return null;
+            }
+
+        }
+
+        public bool SelectRequestedValues(string login, string password, List<string> credentials)
+        {
+            foreach (string line in credentials)
+            {
+                string[] values = line.Split(',');
+                
+                if (values.Length == 2 && values[0].Trim() == login && values[1].Trim() == password)
+                {
+                    Console.WriteLine("Autoryzacja przebiegła pomyślnie.");
+                    return true;
+                }
+            }
+            Console.WriteLine("Nieprawidłowy login lub hasło.");
+            return false;
+        }
+
+        public void AddNewCredentials(string login, string password, string path)
+        {
+            try
+            {
+                using (StreamWriter sw = File.AppendText(filePath))
+                {
+                    sw.WriteLine($"{login},{password}");
+                }
+
+                Console.WriteLine("Nowe dane uwierzytelniające zostały dodane do pliku.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Wystąpił błąd podczas dodawania nowych danych uwierzytelniających: " + ex.Message);
+            }
+        }
+    }
+}
--- CodeDrivers/CodeDrivers/CodeDrivers/Program.cs
+++ CodeDrivers/CodeDrivers/CodeDrivers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CodeDrivers.Repository;
+using System.Security.Claims;
 
 namespace CodeDrivers
 {
@@ -6,12 +7,20 @@ namespace CodeDrivers
     {
         static void Main(string[] args)
         {
-            Menu_tekstowe menu = new Menu_tekstowe("user"); //Na start wyswietla panel menu
+            //Menu_tekstowe menu = new Menu_tekstowe("user"); //Na start wyswietla panel menu
 
-            var carRepo = new CarRepository();
+            //var carRepo = new CarRepository();
             //var allCars = carRepo.GetAll();
             //var availableCars = carRepo.GetAllAvailable();
             //carRepo.DisplayAllItems(availableCars);
+
+            //var csvHandler = new CsvHanlder();
+            //var credentials = csvHandler.GetCredentialsFromFile(@"C:\Users\Janek\Desktop\FakeCrudentials.csv");
+            //csvHandler.SelectRequestedValues("anna.kowalska@gmail.com", "aneczka111", credentials);
+            //csvHandler.AddNewCredentials("jankowalski@gmail.com", "janek1234!", @"C:\Users\Janek\Desktop\FakeCrudentials.csv\");
+
+            CredentialsValidator validator = new CredentialsValidator();
+            CredentialsValidator.ValidatePassword();
         }
     }
 }
