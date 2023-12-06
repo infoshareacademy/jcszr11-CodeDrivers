1008e72128c973b5fce26e4994304dc05c58ccaf
Krzysztof Dabrowski <k.dabrowski87@gmail.com>
2023-12-06 12:13:46 +0100

    remove unnecessary actions

--- CodeDrivers/CodeDrivers/CodeDriversMVC/Controllers/LoginController.cs
+++ CodeDrivers/CodeDrivers/CodeDriversMVC/Controllers/LoginController.cs
@@ -11,12 +11,6 @@ namespace CodeDriversMVC.Controllers
             return View();
         }
 
-        // GET: LoginController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: LoginController/Create
         public ActionResult Create()
         {
@@ -27,48 +21,6 @@ namespace CodeDriversMVC.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: LoginController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: LoginController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: LoginController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: LoginController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
         {
             try
             {
