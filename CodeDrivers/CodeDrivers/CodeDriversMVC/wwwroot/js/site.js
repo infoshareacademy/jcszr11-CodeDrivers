76e291b4d50dd09bbed0a710d8745d7fc3d7cc9a
Krzysztof Dabrowski <k.dabrowski87@gmail.com>
2023-12-06 12:10:23 +0100

    inital for login view

--- CodeDrivers/CodeDrivers/CodeDriversMVC/wwwroot/js/site.js
+++ CodeDrivers/CodeDrivers/CodeDriversMVC/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById("passwordShowCheckbox").addEventListener('change', function () {
+    var passwordField = document.getElementById("passwordTextField");
+    if (this.checked) {
+        passwordField.type = "text";
+    } else {
+        passwordField.type = "password";
+    }
+})
