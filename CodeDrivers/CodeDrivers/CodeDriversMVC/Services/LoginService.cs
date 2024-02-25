0e7fc7524842ac1cb84bf5cae0b7a3061d84102e
Jan Polak <jan.p.polak@gmail.com>
2024-02-25 14:04:07 +0100

    Sesja użytkownika hula

--- CodeDrivers/CodeDrivers/CodeDriversMVC/Services/LoginService.cs
+++ CodeDrivers/CodeDrivers/CodeDriversMVC/Services/LoginService.cs
@@ -1,6 +1,9 @@
 ﻿using CodeDrivers.Models;
 using CodeDriversMVC.DataAccess;
 using CodeDriversMVC.Helpers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -9,20 +12,34 @@ namespace CodeDriversMVC.Services
     public class LoginService
     {
         private readonly CodeDriversContext _context;
+        private readonly IHttpContextAccessor _contextAccessor;
 
-        public LoginService(CodeDriversContext context)
+        public LoginService(CodeDriversContext context, IHttpContextAccessor contextAccessor)
         {
             _context = context;
+            _contextAccessor = contextAccessor;
         }
 
-        public bool AuthorizeUser(string email, string enteredPassword)
+        public async Task<bool> AuthorizeUser(string email, string enteredPassword)
         {
             var searchedUser = _context.Set<User>().FirstOrDefault(user => user.Email == email);
             if (searchedUser == null || !HashPasswordHelper.VerifyPassword(enteredPassword, searchedUser.Password))
             {
                 return false;
             }
+            await _contextAccessor.HttpContext.SignInAsync(CreatePricipal(searchedUser));
             return true;
+        }
+
+        public ClaimsPrincipal CreatePricipal(User user)
+        {
+            var claimsPrincipal = new ClaimsPrincipal();
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, $"{user.Name} {user.LastName}"));
+            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+            ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            claimsPrincipal.AddIdentity(identity);
+            return claimsPrincipal;
         }
     }
 }
