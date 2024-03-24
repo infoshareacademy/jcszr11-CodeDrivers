ecb93e8ad802d289d95c72508bcad86ed9eabcf3
Jan Polak <jan.p.polak@gmail.com>
2024-03-24 12:46:48 +0100

    Mała poprawka

--- CodeDrivers/CodeDrivers/CodeDriversMVC/Controllers/CarSearchController.cs
+++ CodeDrivers/CodeDrivers/CodeDriversMVC/Controllers/CarSearchController.cs
@@ -27,18 +27,14 @@ namespace CodeDriversMVC.Controllers
         // POST: CarSearchController/Reservation
         [HttpPost]
         public ActionResult Reservation(int Id, DateTime startTime, DateTime endTime)
-        {startTime = (startTime < DateTime.Now) ? DateTime.Now : startTime;
-            endTime = (endTime < DateTime.Now) ? DateTime.Now : endTime;
-return RedirectToAction("Create", "Reservation", new { carId = Id, startTime, endTime});
+        {
             startTime = (startTime < DateTime.Now) ? DateTime.Now : startTime;
             endTime = (endTime < DateTime.Now) ? DateTime.Now : endTime;
-
-           
-            return RedirectToAction("Create", "Reservation", new { carId = Id, startTime, endTime});
+            return RedirectToAction("Create", "Reservation", new { carId = Id, startTime, endTime });
 
         }
     }
 }
-        
+
 
 
